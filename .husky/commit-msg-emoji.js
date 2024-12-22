ad838653e1f9d9e3b6c2b068b845065948445b04
Ni0duann <2014567981@qq.com>
2024-12-22 14:24:32 +0800

    feat: auto add emoji

---
+++ .husky/commit-msg-emoji.js
@@ -0,0 +1,27 @@
+import { readFileSync, writeFileSync } from "fs";
+
+const typeToEmojiMap = {
+  chore: "🎨",
+  feat: "✨",
+  fix: "🐛",
+};
+
+const COMMIT_EDITING_FILEPATH = process.argv.at(-1);
+
+const inputMsg = readFileSync(COMMIT_EDITING_FILEPATH, "utf8");
+
+writeFileSync(COMMIT_EDITING_FILEPATH, transform(inputMsg, typeToEmojiMap), "utf8");
+
+function transform(inputMsg, typeToEmojiMap) {
+  const [type, emoji] =
+    Object.entries(typeToEmojiMap).find(([type]) =>
+      inputMsg.startsWith(type),
+    ) ?? [];
+
+  if (type) {
+    return inputMsg.replace(new RegExp(`^${type}`), `${emoji} $&`);
+  }
+
+  throw new Error('Invalid type')
+}
+
