54e10b73b99d74f77c6ee301d411acce009bb562
ni0duann <2014567981@qq.com>
2025-04-30 11:37:38 +0800

    log(note): 2025/04/30

---
+++ 前端手写体集合/划重点/lazyMan.js
@@ -0,0 +1,63 @@
+const { log } = require('console')
+
+class lazyMan {
+  constructor(name) {
+    this.name = name
+    this.tasks = []
+    this.sayHi()
+    setTimeout(() => {
+      this.next()
+    }, 0)
+  }
+
+  sayHi(name) {
+    const task = () => {
+      console.log(`Hi! This is ${this.name}`)
+      this.next()
+    }
+    this.tasks.push(task)
+    return this
+  }
+
+  sleep(time) {
+    const task = () => {
+      setTimeout(() => {
+        console.log(`Wake up after ${time / 1000} seconds`)
+        this.next()
+      }, time * 1000)
+    }
+    this.tasks.push(task)
+    return this
+  }
+
+  sleepFirst(time) {
+    const task = () => {
+      setTimeout(() => {
+        console.log(`Wake up after ${time / 1000} seconds`)
+        this.next()
+      }, time)
+    }
+    this.tasks.unshift(task)
+    return this
+  }
+
+  eat(food) {
+    const task = () => {
+      console.log(`Eat ${food}`)
+      this.next()
+    }
+    this.tasks.push(task)
+    return this
+  }
+
+  next() {
+    const task = this.tasks.shift()
+    task && task()
+  }
+}
+
+function LazyMan(name) {
+  return new lazyMan(name)
+}
+
+LazyMan('jj').sleepFirst(2).eat('lunch').sleep(5).eat('dinner').sleep(3).eat('supper')
